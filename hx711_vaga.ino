35af397dd23372a5ede6f581c6324f627e624b9e
Dejan Petrović <dekip@beotel.rs>
2018-10-04 11:39:32 +0200

    Add files via upload

---
+++ hx711_vaga.ino
@@ -0,0 +1,114 @@
+#include <HX711.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+HX711 scale;
+#define OLED_RESET 4
+Adafruit_SSD1306 display (OLED_RESET);
+const int touchPin = 7;
+int touchState = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(touchPin, INPUT);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+  display.display();
+  Serial.println("HX711 Demo");
+
+  Serial.println("Initializing the scale");
+  // parameter "gain" is ommited; the default value 128 is used by the library
+  // HX711.DOUT  - pin #A1
+  // HX711.PD_SCK - pin #A0
+  scale.begin(A1, A0);
+
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());     // print a raw reading from the ADC
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("KALIBRACIJA");
+  display.setCursor(0,16);
+  display.print("Predpodesavanje");
+  display.display();
+  
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
+
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
+            // by the SCALE parameter (not set yet)
+
+
+  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
+
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("KALIBRACIJA");
+  display.setCursor(0,16);
+  display.print("Podesavanje");
+ 
+  display.display();
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+            // by the SCALE parameter set with set_scale
+
+
+  Serial.println("Readings:");
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("Spremna za rad");
+  display.display();
+  delay(500);
+}
+
+void loop() {
+  touchState = digitalRead(touchPin);
+  Serial.println(touchState);
+  if(touchState == 1){
+    scale.tare(); 
+    }
+  Serial.print("one reading:\t");
+  Serial.print((scale.get_units()*5), 1);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("Tezina u gramima");
+  display.setTextSize(2);
+  display.setCursor(5,25);
+  display.print((scale.get_units()*5), 1);
+  display.print(" g");
+  display.display();
+  Serial.print("\t| average:\t");
+  Serial.println(scale.get_units(10), 1);
+
+  scale.power_down();             // put the ADC in sleep mode
+  delay(100);
+  scale.power_up();
+}
